be12976abe4e4bae1e4803f83e7ad44af9098260
unknown <reda.bekka@gmail.com>
2020-05-19 14:07:16 +0200

    Updates

---
+++ precache-manifest.a0caab15943a2cb0f01494094e4ac524.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "ddad8e795093034d007e",
+    "url": "/static/css/main.b04e3634.chunk.css"
+  },
+  {
+    "revision": "ddad8e795093034d007e",
+    "url": "/static/js/main.ddad8e79.chunk.js"
+  },
+  {
+    "revision": "f4777285ed0c0935e682",
+    "url": "/static/js/1.f4777285.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b9a86c50cc3fe786eb584dd4dc784caf",
+    "url": "/static/media/default.b9a86c50.jpeg"
+  },
+  {
+    "revision": "4200b6b96b6c171e52c4a97d34a1174e",
+    "url": "/static/media/chocolat.4200b6b9.jpeg"
+  },
+  {
+    "revision": "815852b98cfd976880da9f88ecc5892e",
+    "url": "/static/media/epinard.815852b9.jpg"
+  },
+  {
+    "revision": "fca17319421519cbb9d73c85b4879566",
+    "url": "/static/media/hamburger.fca17319.jpeg"
+  },
+  {
+    "revision": "287d53ae5223bf74627baa68ecc5f5c1",
+    "url": "/static/media/pizza.287d53ae.jpeg"
+  },
+  {
+    "revision": "e7b3552cad0c162a9f35eabe72fe643e",
+    "url": "/static/media/qualb.e7b3552c.jpg"
+  },
+  {
+    "revision": "8324a6142c5fc9d0bdd0b453c162f9a1",
+    "url": "/static/media/saumon.8324a614.jpeg"
+  },
+  {
+    "revision": "96c2f7548bab68d96c489276dbc837e6",
+    "url": "/static/media/tartine.96c2f754.jpg"
+  },
+  {
+    "revision": "e3b9779c0acf126b8e9af7b2fb200520",
+    "url": "/index.html"
+  }
+];
